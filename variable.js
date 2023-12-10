3cba1df7d7eb881b1e8d9207b183c70cc8491576
hetashridk <hetashrik@gmail.com>
2023-12-10 13:54:16 +0530

    Variable file added

---
+++ variable.js
@@ -0,0 +1,14 @@
+const accountId = 123
+let accoumtEmail = "hetashri@gmail.com"
+var accountPassword = "456"
+accountCity = "Surat"
+let accountState;
+
+// accountId = 978
+
+accoumtEmail = "hk@gmail.com"
+accountPassword = "4457"
+accountCity = "Navsari"
+console.log(accountId);
+
+console.table([accountId, accoumtEmail, accountPassword, accountCity, accountState]);
