d6bcd648b0a3b851f254c8b25909d4d344f65729
hetashridk <hetashrik@gmail.com>
2023-12-10 15:28:38 +0530

    operation file  added

---
+++ operations.js
@@ -0,0 +1,25 @@
+let val = 3;
+let negval = -val;
+console.log(negval);
+
+let str1 = "hello"
+let str2 = " hetashri"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log(1 + 2);   //3
+console.log("1" + 2); //12
+console.log(1 + "2"); //12 
+console.log(1 + 1 + "2");  //22 -> If first is addition then it will take
+console.log("1" + 1 + 2);  //112 -> If first is string then it will do it in form of string
+
+
+let counter = 10
+++counter;
+console.log(counter); //output 11
+
+let count = 10
+count++;
+console.log(count); //output 11
