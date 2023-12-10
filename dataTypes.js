8ae64e25af0ce5b24cf7aef6ea63720df4413e1a
hetashridk <hetashrik@gmail.com>
2023-12-10 15:03:13 +0530

    dataTypes file added

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//Primitive 
+let name = "Hetashri";
+let age = 20;
+let login = false;
+
+//null is a standalone value:- empty value
+//undefiend:- value not assign
+//symbol:- to find uniqueness
+
+//------------------------------------------------------//
+
+
+// Object
+
+console.log(typeof "Hetu");
+console.log(typeof age);
+console.log(typeof null); //o/p:- object
+console.log(typeof undefined); //o/p:- undefined
