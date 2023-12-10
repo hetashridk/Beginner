85c43d83d033a6dcf8c1812a0137b4b029c7912b
hetashridk <hetashrik@gmail.com>
2023-12-10 15:17:30 +0530

    Conversion of datatype added

--- dataTypes.js
+++ dataTypes.js
@@ -18,3 +18,60 @@ console.log(typeof "Hetu");
 console.log(typeof age);
 console.log(typeof null); //o/p:- object
 console.log(typeof undefined); //o/p:- undefined
+
+console.log("----------------------------");
+
+// Conversion of Datatypes
+let score = "33"
+console.log(typeof score);
+let scoreInNumber = Number(score);  //Convert from string to number
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber);
+
+console.log("----------------------------");
+
+// Conversion of Datatypes
+let scored = "33aab"
+console.log(typeof scored);
+let scoreInNumbers = Number(scored);  //Convert from string to number
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber);
+
+console.log("----------------------------");
+
+// Conversion of Datatypes
+let scoredd = "null"
+console.log(typeof scoredd);
+let scoreInNumberss = Number(scoredd);  //Convert from string to number
+console.log(typeof scoreInNumberss);
+console.log(scoreInNumberss);
+
+console.log("----------------------------");
+
+// Conversion of Datatypes
+let scoreddd = 0
+console.log(typeof scoreddd);
+let scoreInNumbersss = Number(scoreddd);  //Convert from string to number
+console.log(typeof scoreInNumbersss);
+console.log(scoreInNumbersss);
+
+console.log("----------------------------");
+
+// Conversion of Datatypes
+let scoredddd = undefined
+console.log(typeof scoredddd);
+let scoreInNumberssss = Number(scoredddd);  //Convert from string to number
+console.log(typeof scoreInNumberssss);
+console.log(scoreInNumberssss);
+
+console.log("----------------------------");
+
+// Conversion of Datatypes
+let scoreddddd = true
+console.log(typeof scoreddddd);
+let scoreInNumbersssss = Number(scoreddddd);  //Convert from string to number
+console.log(typeof scoreInNumbersssss);
+console.log(scoreInNumbersssss);
+
+
+// Conversion can be Number, String, Boolean
