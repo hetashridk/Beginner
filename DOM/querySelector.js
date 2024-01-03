42a9b19f261d0d774704b4284f76e529aefca148
hetashridk <hetashrik@gmail.com>
2024-01-03 22:46:30 +0530

    DOM:- Query Selector added

---
+++ DOM/querySelector.js
@@ -0,0 +1,18 @@
+
+// it will give output the first h2 tag
+// document.querySelector('h2') => <h2>Lorem ipsum dolor sit amet consectetur adipisicing.</h2>
+
+// we can also get id
+// document.querySelector('#title') => <h1 id="title" class="heading">Welcome to DOM</h1>
+
+// we can also get classs
+// document.querySelector('.heading') => <h1 id="title" class="heading">Welcome to DOM</h1>
+
+// we can also get input
+// document.querySelector('input') => <input type="password" name="pass" id="password">
+// document.querySelector('input[type = "password"]') => <input type="password" name="pass" id="password">
+
+// it will also works on list
+// document.querySelector('ul') => will give you the list
+// const myLi = document.querySelector('li')
+// myLi.style.backgroungColor = 'orange' => will change color of very first list
