247714f7757ed94f4a5a8465bb60ff3e86841007
hetashridk <hetashrik@gmail.com>
2024-01-23 22:19:19 +0530

    Something about inheritance added

---
+++ staticProp.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const jhone = new User("jhone")
+// console.log(hitesh.createId())   //we can't access
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
